9cddf5f8d68543b44557f63e73834494a0c15c81
haykarshakyan <haykarshakyan@hotmail.com>
2023-12-05 20:55:53 +0400

    learn7

---
+++ learn7/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const app = express();
+
+
+app.get("/data", (req, res) => {
+    console.log("pppp");
+    res.send({
+        firstName:"Jon",
+        lastname:"Jecson"
+    })
+    // console.log(process.env.PORT);
+})
+
+
+
+app.listen(process.env.PORT || 3001);
+
